eb4f863662d16b5d647da0eff1a3611026370458
OriMiller <OriMiller@users.noreply.github.com>
2017-01-25 11:05:46 -0600

    Add files via upload

--- Client.java
+++ Client.java
@@ -4,6 +4,7 @@ import java.awt.event.ActionListener;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 import java.awt.event.WindowListener;
+
 import java.io.BufferedOutputStream;
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
@@ -11,6 +12,7 @@ import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.net.ConnectException;
 import java.net.Socket;
 import java.net.UnknownHostException;
 
@@ -29,27 +31,29 @@ import javax.swing.JScrollPane;
  */
 public class Client {
 		private static String ip = "127.0.0.1";
+		private static Socket socket;
 		private static File file;
 		private static int bytesRead;
 	    private static int current;
-	    private static FileOutputStream fos = null;
-	    private static BufferedOutputStream bos = null;
-		private static Socket socket;
+	    // private static FileOutputStream fos = null;
+	    // private static BufferedOutputStream bos = null;
+	    // private static InputStream is = null;
 		private static DataInputStream in;
 		private static DataOutputStream out;
 		private static String[] filelist;
 		private static JButton refresh = new JButton("Refresh");
 		private static JButton download = new JButton("Download");
+		private static JButton home = new JButton("Home");
 		private static JList<String> list = new JList<String>();
 		private static JScrollPane sPane = new JScrollPane();
 		private static JPanel panel = new JPanel();
 		private static JPanel buttonPanel = new JPanel();
-		private static JFrame frame = new JFrame("File Transfer Client" + ip);
+		private static JFrame frame = new JFrame("Java File Transfer Client");
 		private final static String FILE_TO_RECEIVED_ORG = "/Users/orimiller/TEMP1/";
 		public static String FILE_TO_RECEIVED = "/Users/orimiller/TEMP1/";
 		
 	public Client() throws UnknownHostException, IOException{
-		
+		try{
 		socket = new Socket(ip,2525);
 	    in = new DataInputStream(socket.getInputStream());
 	    out = new DataOutputStream(socket.getOutputStream());
@@ -64,10 +68,11 @@ public class Client {
 		sPane.getViewport().add(list);
 		frame.add(buttonPanel);
 		buttonPanel.add(refresh);
+		buttonPanel.add(home);
 		buttonPanel.add(download);		
-		//frame.setSize(400, 400);
 		frame.pack();
 		updateList();
+		
 		refresh.addActionListener(new ActionListener(){
 			public void actionPerformed(ActionEvent e) {
 				updateList();
@@ -78,37 +83,12 @@ public class Client {
 				try{
 					int get = list.getSelectedIndex();
 					if(get != -1 && (list.getSelectedValue().endsWith("(1)"))){
-						current = 0;
-						out.writeUTF("SEND");
-						out.writeInt(get);
-						int gets = in.readInt();
-						FILE_TO_RECEIVED += list.getSelectedValue().substring(0,(list.getSelectedValue().length()-3));
-						System.out.println(FILE_TO_RECEIVED);
-						file = new File(FILE_TO_RECEIVED);
-						file.createNewFile();
-					    byte [] mybytearray  = new byte [gets];
-					    InputStream is = socket.getInputStream();
-					    fos = new FileOutputStream(FILE_TO_RECEIVED);
-					    bos = new BufferedOutputStream(fos);
-					    bytesRead = is.read(mybytearray,0,mybytearray.length);
-					    current = bytesRead;
-					    if(current < mybytearray.length){
-						    do{
-							    bytesRead = is.read(mybytearray, current, mybytearray.length - current);
-							    if(bytesRead > 0){
-							    current += bytesRead;
-							    }
-							    System.out.println(current);
-						    } while(bytesRead > 0); 
-					    } 
-					    bos.write(mybytearray, 0 , current);
-					    bos.flush();
-					    System.out.println("File " + FILE_TO_RECEIVED + " downloaded (" + current + " bytes read)"); 
+						 download(get);
 					} else if(get != -1){
 						out.writeUTF("SEND");
 						out.writeInt(get);
 						FILE_TO_RECEIVED += list.getSelectedValue().substring(0,(list.getSelectedValue().length()-3)) + "/";
-						File file = new File(FILE_TO_RECEIVED);
+						file = new File(FILE_TO_RECEIVED);
 						if(!file.exists()){
 							file.mkdir();
 							updateList();
@@ -119,30 +99,69 @@ public class Client {
 				}
 			}
 		});
+		home.addActionListener(new ActionListener(){
+			public void actionPerformed(ActionEvent e) {
+					try {
+						out.writeUTF("HOME");
+						FILE_TO_RECEIVED = FILE_TO_RECEIVED_ORG;
+						updateList();
+					} catch (IOException e1) {
+						e1.printStackTrace();
+					}		
+			}
+		});
 		WindowListener exitListener = new WindowAdapter() {
 		    @Override
 		    public void windowClosing(WindowEvent e) {
-		        int confirm = JOptionPane.showOptionDialog(
-		             null, "Are You Sure to Close Application?", 
-		             "Exit Confirmation", JOptionPane.YES_NO_OPTION, 
-		             JOptionPane.QUESTION_MESSAGE, null, null, null);
-		        if (confirm == 0) {
-		        	try {
-						out.writeUTF("CLOSING");
-					} catch (IOException e1) {
-						e1.printStackTrace();
-					}
-		           System.exit(0);
+		        try{
+		        	out.writeUTF("CLOSING");
+		        } catch(Exception e1){
+		        	
 		        }
-		    }
+		    } 
 		};
 		frame.addWindowListener(exitListener);
+		} catch(ConnectException e){
+			e.printStackTrace();
+			JOptionPane.showMessageDialog(new JFrame(), "Couldn't Connect to " + ip + ", Please try again later");
+			System.exit(0);
+		}
 	}
 	private void run() throws Exception{
 		while(true){
 			
 		}
     }
+	private static void download(int selectedValue) throws IOException{
+		String temp = FILE_TO_RECEIVED + list.getSelectedValue().substring(0,(list.getSelectedValue().length()-3));
+		if(!(new File(temp).exists())){
+		current = 0;
+		out.writeUTF("SEND");
+		out.writeInt(selectedValue);
+		int gets = in.readInt();
+		System.out.println(temp);
+		new File(temp).createNewFile(); //creates an empty file with the name of the selected file
+	    byte [] mybytearray  = new byte [gets]; //creates empty byte array with the size sent from the server
+	    InputStream is = socket.getInputStream();
+	    FileOutputStream fos = new FileOutputStream(temp);
+	    BufferedOutputStream bos = new BufferedOutputStream(fos);
+	    bytesRead = is.read(mybytearray,0,mybytearray.length);
+	    current = bytesRead;
+	    if(current < mybytearray.length){
+		    do{
+			    bytesRead = is.read(mybytearray, current, mybytearray.length - current);
+			    if(bytesRead > 0){
+			    current += bytesRead;
+			    }
+			    System.out.println(current);
+		    } while(bytesRead > 0); 
+	    } 
+	    bos.write(mybytearray, 0 , current);
+	    bos.flush();
+	    System.out.println("File " + temp + " downloaded (" + current + " bytes read)");
+	    bos.close();
+		}
+	}
 	private static void updateList(){
 		try {
 			out.writeUTF("REFRESH");
@@ -158,7 +177,6 @@ public class Client {
 	public static void main(String[] args) throws Exception {
 		try{
 			Client client = new Client();
-			
 			frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 			frame.setVisible(true);
 			client.run();
